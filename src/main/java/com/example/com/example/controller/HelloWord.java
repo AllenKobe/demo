951dbfc89206cdfa49566d734fbef34907cd9d9a
dcc1996 <1179696362@qq.com>
2018-07-05 10:13:33 +0800

    杜聪聪

--- src/main/java/com/example/com/example/controller/HelloWord.java
+++ src/main/java/com/example/com/example/controller/HelloWord.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWord {
     @RequestMapping("/test")
     public String test(){
-        return "hello word 123123";
+        return "hello world 123123";
     }
 }
