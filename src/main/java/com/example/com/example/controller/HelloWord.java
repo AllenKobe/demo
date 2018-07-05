7b39c4e9bdef5e6c1dc41377828b5a01e0b71d7f
dcc1996 <1179696362@qq.com>
2018-07-05 11:01:30 +0800

    jsp

--- src/main/java/com/example/com/example/controller/HelloWord.java
+++ src/main/java/com/example/com/example/controller/HelloWord.java
@@ -1,12 +1,20 @@
 package com.example.com.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWord {
+    @ResponseBody
     @RequestMapping("/test")
     public String test(){
         return "hello world她她她执行政策  SDSSS 123123";
     }
+    @RequestMapping("/main")
+    public String main(){
+        return "hello.jsp";
+    }
+
 }
